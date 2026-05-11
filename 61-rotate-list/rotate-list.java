5f81624a16cb59a30c8cf686c591d0ba9996445f
Sai Laxmi Pasagadugula <23a91a4446@aec.edu.in>
2026-05-11 17:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.69%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        // Edge cases
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        // Find length of list
+        int length = 1;
+        ListNode tail = head;
+
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        // Reduce unnecessary rotations
+        k = k % length;
+
+        // If no rotation needed
+        if (k == 0) {
+            return head;
+        }
+
+        // Make circular linked list
+        tail.next = head;
+
+        // Find new tail
+        int stepsToNewTail = length - k;
+
+        ListNode newTail = head;
+
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // New head
+        ListNode newHead = newTail.next;
+
+        // Break the circle
+        newTail.next = null;
+
+        return newHead;
+    }
+}
